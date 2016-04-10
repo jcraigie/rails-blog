94c57955a85c4cf9a9e99e77b00a6a5f8d1506ab
John Craigie <email": null>
2016-04-10 13:23:17 -0700

    Add delete of articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(parms[:id])
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -29,13 +29,18 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
 
-    if =@article.update(article_params)
+    if @article.update(article_params)
       redirect_to @article
     else
       render 'edit'
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   private
